0ea244cbfe54be9324e6f7c654b44ebbb735b41e
jmargielewski <kuba.margielewski@gmail.com>
2020-12-27 19:23:08 +0000

    enzyme stup

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import Enzyme from 'enzyme';
 import '@testing-library/jest-dom';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
